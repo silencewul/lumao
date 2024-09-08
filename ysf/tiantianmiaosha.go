3a1206b21f70ff11d8c8aed6a37f48aea838f791
silencewul <wulsilence@gmail.com>
2024-09-09 00:24:18 +0800

    up

--- ysf/tiantianmiaosha.go
+++ ysf/tiantianmiaosha.go
@@ -22,13 +22,13 @@ func Strat(num int) string {
 	)
 
 	c.OnRequest(func(r *colly.Request) {
-		//r.Headers.Set("cookie", "route=68507c350d203ede9aba382a4e62a92b;sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2219127296f1b299-03e5fffd6347818-227f031e-329160-19127296f1c544%22%2C%22%24device_id%22%3A%2219127296f1b299-03e5fffd6347818-227f031e-329160-19127296f1c544%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer_host%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfYW5vbnltb3VzX2lkIjoiMTkxMjcyOTZmMWIyOTktMDNlNWZmZmQ2MzQ3ODE4LTIyN2YwMzFlLTMyOTE2MC0xOTEyNzI5NmYxYzU0NCIsIiRpZGVudGl0eV9jb29raWVfaWQiOiIxOTE3ZGZlN2IxNDI3MzgtMGVjODc2OGRiYjA3ZWYtM2QyNTU3NDUtMzI5MTYwLTE5MTdkZmU3YjE1MmE1ZCJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%7D")
+		r.Headers.Set("cookie", "route=68507c350d203ede9aba382a4e62a92b;sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2219127296f1b299-03e5fffd6347818-227f031e-329160-19127296f1c544%22%2C%22%24device_id%22%3A%2219127296f1b299-03e5fffd6347818-227f031e-329160-19127296f1c544%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer_host%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfYW5vbnltb3VzX2lkIjoiMTkxMjcyOTZmMWIyOTktMDNlNWZmZmQ2MzQ3ODE4LTIyN2YwMzFlLTMyOTE2MC0xOTEyNzI5NmYxYzU0NCIsIiRpZGVudGl0eV9jb29raWVfaWQiOiIxOTE3ZGZlN2IxNDI3MzgtMGVjODc2OGRiYjA3ZWYtM2QyNTU3NDUtMzI5MTYwLTE5MTdkZmU3YjE1MmE1ZCJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%7D")
 		r.Headers.Set("Accept", "*/*")
 		r.Headers.Set("Accept-Encoding", "gzip, deflate, br")
 
 		r.Headers.Set("Accept-Language", "zh-CN,zh-Hans;q=0.9")
 
-		//r.Headers.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjE0MzMzMzc0MzkzNTUzOTMsInJvbGUiOiJVTklQQVlfVVNFUiIsInBydiI6IjUwMDIzZjllMDlmYWRiYzhjZDhhMmQ2NDBjZDkwZWI3OWUzYjQ2ZjgiLCJncmQiOiJvYXV0aCIsImlhdCI6MTcyNTc1NzI2MSwiZXhwIjoxNzI1ODQzNjYxfQ.BTlcU1pAGmlF1dKXvKXf9CLrc1x8K9x3PNHWxMgk-_I")
+		r.Headers.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjE0MzMzMzc0MzkzNTUzOTMsInJvbGUiOiJVTklQQVlfVVNFUiIsInBydiI6IjUwMDIzZjllMDlmYWRiYzhjZDhhMmQ2NDBjZDkwZWI3OWUzYjQ2ZjgiLCJncmQiOiJvYXV0aCIsImlhdCI6MTcyNTc1NzI2MSwiZXhwIjoxNzI1ODQzNjYxfQ.BTlcU1pAGmlF1dKXvKXf9CLrc1x8K9x3PNHWxMgk-_I")
 
 		r.Headers.Set("app", "cqylcs")
 		r.Headers.Set("Referer", "https://cqbdshzq.cup.com.cn/wap/cqylcs/home/up/")
@@ -56,8 +56,8 @@ func Strat(num int) string {
 			body := new(TResponse)
 			json.Unmarshal(r.Body, body)
 			log.Println(body)
-			//Res = fmt.Sprintf("%s -------- %f", body.Name, body.SalesVol)
-			Res = string(r.Body)
+			Res = fmt.Sprintf("%s -------- %f", body.Name, body.SalesVol)
+			//Res = string(r.Body)
 		}
 		Res = "请求失败"
 	})

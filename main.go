67f25cdeb6b0a77421b3e7faecc4c5faf57cc0c6
silencewul <wulsilence@gmail.com>
2024-09-08 23:27:48 +0800

    up

--- main.go
+++ main.go
@@ -13,5 +13,5 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run("8088") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
